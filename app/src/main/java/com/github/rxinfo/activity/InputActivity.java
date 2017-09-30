91d2c07a4856c8785d108627adfb018222d8c5df
Corey Rowe <coreyr1196@gmail.com>
2017-09-30 12:42:33 -0400

    Create activity package

--- app/src/main/java/com/github/rxinfo/activity/InputActivity.java
+++ app/src/main/java/com/github/rxinfo/activity/InputActivity.java
@@ -1,13 +1,15 @@
-package com.github.rxinfo;
+package com.github.rxinfo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.rxinfo.R;
+
+public class InputActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_input);
     }
 }
