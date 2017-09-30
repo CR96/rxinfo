7132a58d89e544a7f357ecfac280c63f5ce953ef
Corey Rowe <coreyr1196@gmail.com>
2017-09-30 18:24:22 -0400

    Scan barcodes

--- app/src/main/java/com/github/rxinfo/activity/InputActivity.java
+++ app/src/main/java/com/github/rxinfo/activity/InputActivity.java
@@ -1,10 +1,11 @@
 package com.github.rxinfo.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +18,7 @@ import com.github.rxinfo.R;
 public class InputActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CAMERA = 1;
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_BARCODE_TEXT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,33 +35,46 @@ public class InputActivity extends AppCompatActivity {
                 if (ContextCompat.checkSelfPermission(
                         InputActivity.this, Manifest.permission.CAMERA)
                         != PackageManager.PERMISSION_GRANTED) {
-
                     ActivityCompat.requestPermissions(InputActivity.this,
                             new String[]{Manifest.permission.CAMERA},
                             PERMISSION_REQUEST_CAMERA);
+                }else{
+                    Intent scanBarcodeIntent = new Intent(
+                            InputActivity.this,
+                            ScanActivity.class);
+
+                    startActivityForResult(scanBarcodeIntent, REQUEST_BARCODE_TEXT);
                 }
             }
         });
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_BARCODE_TEXT && resultCode == Activity.RESULT_OK) {
+
+        }
+    }
+
+    @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
         switch (requestCode) {
             case PERMISSION_REQUEST_CAMERA: {
+
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent scanBarcodeIntent = new Intent(
+                            InputActivity.this,
+                            ScanActivity.class);
 
-                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    }
-
+                    startActivityForResult(scanBarcodeIntent, REQUEST_BARCODE_TEXT);
                 } else {
                     Toast.makeText(
                             InputActivity.this,
-                            getString(R.string.permission_camery_denied),
+                            getString(R.string.permission_camera_denied),
                             Toast.LENGTH_LONG).show();
                 }
             }
---
+++ app/src/main/java/com/github/rxinfo/activity/ScanActivity.java
@@ -0,0 +1,115 @@
+package com.github.rxinfo.activity;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.github.rxinfo.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        setTitle("Scan a barcode");
+
+        final SurfaceView cameraPreview = findViewById(R.id.cameraPreview);
+        final TextView txtBarcode = findViewById(R.id.txtBarcode);
+        final Button btnGo = findViewById(R.id.btnGo);
+
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        final CameraSource cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @SuppressLint("MissingPermission") // Permission accepted before activity launches
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+
+                SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() > 0) {
+                    final Barcode barcode = barcodes.valueAt(0); // Get the first one
+                    txtBarcode.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String ndc = correctNdc(barcode.rawValue);
+                            String text =  String.format(
+                                    getString(R.string.scanner_barcode_detected),
+                                    ndc
+                            );
+
+                            txtBarcode.setText(text);
+                            btnGo.setVisibility(View.VISIBLE);
+
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    /**
+     * Remove the first and last digit from the barcode, and format with dashes.
+     * @param rawValue The value read from the barcode
+     */
+    private String correctNdc(String rawValue) {
+        try {
+            String fixedValue = rawValue.substring(1, rawValue.length() - 1)
+                    .substring(0, rawValue.length() - 2); // Remove first and last digit
+
+            fixedValue = fixedValue.substring(0, 4) + "-" +
+                    rawValue.substring(5, 9) + "-" + rawValue.substring(9, 11); // Format with dashes
+            return fixedValue;
+        }catch (IndexOutOfBoundsException e) {
+            // Just leave it as is
+            return rawValue;
+        }
+    }
+}
